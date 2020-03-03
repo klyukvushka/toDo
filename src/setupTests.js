ef236ab6aef85646af4abf12e271a785d077f778
klyukvushka <katyalex103@gmail.com>
2020-03-03 17:29:06 +0200

    tests setup

---
+++ src/setupTests.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+import raf from "./test-shim";
+
+import { configure, shallow, render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
