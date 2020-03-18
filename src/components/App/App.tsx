4f01b616f311c316a7503d384d498292f3c32814
klyukvushka <katyalex103@gmail.com>
2020-03-18 17:26:13 +0200

    adaptive version

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,15 +4,13 @@ import Form from "../Form/Form";
 import { List } from "../List/List";
 import ActionPanel from "../ActionPanel/ActionPanel";
 
-import "./App.scss";
-
 export const App: React.FC = () => {
   return (
     <main>
       <div className="container">
         <Title className="title_main">Your ToDo List</Title>
         <Form />
-        <Title>List</Title>
+        <h2 className="title">List</h2>
         <ActionPanel />
         <List />
       </div>
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import classNames from "classnames";
-import "./Input.scss";
 
 type Props = {
   label: string;
